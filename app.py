39ed67c01316847148f8151e369f294015203c58
Jash <jash.rashne@gmail.com>
2025-05-08 14:31:45 +0530

    test karo

--- app.py
+++ app.py
@@ -29,8 +29,9 @@ CORS(
         "http://localhost:3000",
         "https://psymoat.vercel.app",
         "https://www.psymoat.in",
+        "https://psymoat.in",
         "https://psymoat-9w5leylea-cogvergedevs-projects.vercel.app",
-        "https://proxy-psymoat.vercel.app"
+        "https://proxy-psymoat.vercel.app",
         "https://www.psymoat.in/api"
     ],
     supports_credentials=True
